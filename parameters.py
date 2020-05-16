9f22d4e04b01ead2be21ddbeb55d97650fbadf9d
anukulrathore <anukulrthr259@gmail.com>
2020-05-17 00:03:50 +0530

    My first commit

---
+++ parameters.py
@@ -0,0 +1,9 @@
+search = 'ENTER QUERY' #FORMAT: 'COMPUTER DEVELOPER IN NEW YORK'
+username = 'ENTER USERNAME' #FORMAT:'ABC@GMAIL.COM'
+password = 'ENTER PASSWORD' #FORMAT:'PASSWORD'
+
+
+
+#Don't change below code
+search_query = 'site:linkedin.com'+search
+result_file = 'output.csv'
